4fe075cb4354e41fbb41649e08d778ac0137cd51
Jeff St. Jean <jeffrey@stjean.ca>
2020-09-11 03:57:55 -0400

    cookie

--- api/services/Auth.js
+++ api/services/Auth.js
@@ -53,7 +53,7 @@ const is_user = async (req, res, next) => {
         const { user, err } = await decode_user(req)
         if(err) {
             res.clearCookie('auth')
-            req.cookie('next', req.url)
+            res.cookie('next', req.url)
             res.redirect('login')
         }
         else {
